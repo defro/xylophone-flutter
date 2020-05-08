8b438ca993ffe1c38bba8c12df069ed14e874b1a
Joël Gaujard <j.gaujard@gmail.com>
2020-05-08 16:23:12 +0200

    Customize it with some instrument

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -9,7 +10,7 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Expanded buildKey({int soundNumber, Color color}) {
+  Widget buildKey({int soundNumber, Color color}) {
     return Expanded(
       child: FlatButton(
         onPressed: () {
@@ -20,22 +21,65 @@ class XylophoneApp extends StatelessWidget {
     );
   }
 
+  Widget buildKeyInstrument({String soundName, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          final player = AudioCache();
+          player.play('$soundName.wav');
+        },
+        color: color,
+        child: Text(soundName),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Row(
             children: <Widget>[
-              buildKey(soundNumber: 1, color: Colors.red),
-              buildKey(soundNumber: 2, color: Colors.orange),
-              buildKey(soundNumber: 3, color: Colors.yellow),
-              buildKey(soundNumber: 4, color: Colors.blue),
-              buildKey(soundNumber: 5, color: Colors.teal),
-              buildKey(soundNumber: 6, color: Colors.green),
-              buildKey(soundNumber: 7, color: Colors.purple),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    buildKey(soundNumber: 1, color: Colors.red),
+                    buildKey(soundNumber: 2, color: Colors.orange),
+                    buildKey(soundNumber: 3, color: Colors.yellow),
+                    buildKey(soundNumber: 4, color: Colors.blue),
+                    buildKey(soundNumber: 5, color: Colors.teal),
+                    buildKey(soundNumber: 6, color: Colors.green),
+                    buildKey(soundNumber: 7, color: Colors.purple),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    buildKeyInstrument(
+                        soundName: 'violin', color: Colors.red.shade200),
+                    buildKeyInstrument(
+                        soundName: 'flute', color: Colors.orange.shade200),
+                    buildKeyInstrument(
+                        soundName: 'sax', color: Colors.yellow.shade200),
+                    buildKeyInstrument(
+                        soundName: 'trumpet', color: Colors.blue.shade200),
+                    buildKeyInstrument(
+                        soundName: 'aah', color: Colors.teal.shade200),
+                    buildKeyInstrument(
+                        soundName: 'guitar', color: Colors.green.shade200),
+                    buildKeyInstrument(
+                        soundName: 'cello', color: Colors.purple.shade200),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
